25f589f33ef4ce5f8c0bb4a07471166a70764ac8
Damian <damian.szmulik@capgemini.com>
2021-08-06 09:32:30 +0200

    Clean core project

--- ForumApp.Core/Services/PostsService.cs
+++ ForumApp.Core/Services/PostsService.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ForumApp.Core.Interfaces;
 using ForumApp.Core.Models;
-using RestSharp;
 
 namespace ForumApp.Core.Services
 {
     public class PostsService:RestService, IJsonPlaceHolderService<Posts>
     {
-        readonly IRestClient _restClient;
-        readonly IRestRequest _restRequest;
-        private string path = "/post";
+
+        private string path = "/posts";
 
 
         public PostsService()
         {
-            _restClient = CreateRestClient();
-            _restRequest = CreateRestRequest(path);
+
         }
 
         public Posts Get()
         {
-            
+            return null;
         }
 
         public List<Posts> GetAll()
         {
-            var postsList = CreateRestResponse(_restClient, _restRequest);
+            return null;
         }
     }
 }
