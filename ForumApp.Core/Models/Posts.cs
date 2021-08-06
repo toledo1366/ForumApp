4f1b323c908c088b24cb748f6a3d0c8b2c9d1c1c
Damian <damian.szmulik@capgemini.com>
2021-08-06 08:26:55 +0200

    Added model for posts

---
+++ ForumApp.Core/Models/Posts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ForumApp.Core.Models
+{
+    public class Posts
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
